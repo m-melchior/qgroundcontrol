a48e57a04a3feaa7fcc65219c2c4f216b9872005
Michael Melchior <melchiormichael@web.de>
2021-05-27 10:02:44 +0200

    Added upstream docker files note: the command line is docker run --rm -v $PWD:/project/source -v $PWD/build:/project/build qgc-linux-docker added two helper scripts - docker_build.sh to build via docker (in my case the results end up in /tmp, which is a ram drive on my machine to reduce wear and tear on my SSD) - remove_submodules.sh since I had some issues on updating the submodules when switching branches

---
+++ docker_build.sh
@@ -0,0 +1,1 @@
+docker run --rm -v /home/michael/workspaces/PX4/qgroundcontrol:/project/source -v /tmp/build:/project/build qgc-linux-docker
---
+++ remove_submodules.sh
@@ -0,0 +1,4 @@
+rm libs/OpenSSL -R
+rm libs/mavlink -R
+rm libs/qmlsink/hst-plugins-good -R
+rm src/GPS/Drivers -R
